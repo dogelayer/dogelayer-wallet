15fe40b189f639b608eebef56292c5f105cc7f07
Wind Wind <wind@dogelayer.org>
2023-11-27 20:25:46 +0800

    hide 2fa wallet

--- electrum/gui/qt/wizard/wallet.py
+++ electrum/gui/qt/wizard/wallet.py
@@ -390,7 +390,7 @@ class WCWalletType(WizardComponent):
         message = _('What kind of wallet do you want to create?')
         wallet_kinds = [
             ('standard',  _('Standard wallet')),
-            ('2fa',       _('Wallet with two-factor authentication')),
+            # ('2fa',       _('Wallet with two-factor authentication')),
             ('multisig',  _('Multi-signature wallet')),
             ('imported',  _('Import Bitcoin addresses or private keys')),
         ]
